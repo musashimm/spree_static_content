d8f60ef73d0c8e988f868817fb361f4052b01134
Wojciech Todryk <wojciech@todryk.pl>
2017-09-17 15:03:22 +0200

    add created page to current store

--- app/controllers/spree/admin/pages_controller.rb
+++ app/controllers/spree/admin/pages_controller.rb
@@ -1,6 +1,13 @@
 module Spree
   module Admin
     class PagesController < ResourceController
+      create.after :add_page_to_current_store
+
+      private
+
+      def add_page_to_current_store
+        @object.stores << current_store
+      end
     end
   end
 end
